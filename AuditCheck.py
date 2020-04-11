46964518649537bdf59e7332ce83f8e0aeaf1615
Prashant Abkari <prashant@aptira.com>
2020-04-11 14:46:44 +1000

    AWS Modified

--- AuditCheck.py
+++ AuditCheck.py
@@ -1,25 +1,51 @@
 import logging
 import requests
 import json
-import jsonify
+import boto3
+
+
 
 from msal import ConfidentialClientApplication
 
-app = ConfidentialClientApplication("871d759b-b74e-46b8-81c5-076d7ec70426",
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    #Get Client ID and Secret
+    client = boto3.client('ssm')
+    response = client.get_parameters(
+            Names=[
+                "AzureClientCredentials",
+                "AzureClientId"
+            ],
+            WithDecryption=False
+        )
+    
+    for parameter in response['Parameters']:
+        if parameter['Name'] == "AzureClientCredentials":
+            clientsecret = parameter['Value']
+        elif parameter['Name'] == "AzureClientId":
+            clientid = parameter['Value']
+            
+    app = ConfidentialClientApplication(clientid,
                                     authority="https://login.microsoftonline.com/1d73b912-1284-48b2-be6c-c5121b868d2d",
-                                    client_credential="rZU2:.3Zr=lKt/W7ORCG3=Og6UC8V1yN")
-
-result = None
-result = app.acquire_token_silent(["https://graph.microsoft.com/.default"], account=None)
-
-if not result:
-    logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
-    result = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
-
-if "access_token" in result:
-    print(result['access_token'])
-    user_data = requests.get(  # Use token to call downstream service
-        "https://graph.microsoft.com/v1.0//auditLogs/directoryaudits",
-        headers={'Authorization': 'Bearer ' + result['access_token']}, ).json()
+                                    client_credential=clientsecret)
+                                    
+    result = None
+    result = app.acquire_token_silent(["https://graph.microsoft.com/.default"], account=None)
+    
+    if not result:
+        logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+        result = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
+    if "access_token" in result:
+        print(result['access_token'])
+        user_data = requests.get("https://graph.microsoft.com/v1.0/users",
+                                headers={'Authorization': 'Bearer ' + result['access_token']}, ).json()
 
     print(user_data)
+                                    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
