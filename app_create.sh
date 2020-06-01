645ebd6321bb346629e05662a233e88393469ce3
Prashant Abkari <prashant@aptira.com>
2020-06-01 12:19:19 +1000

    Removed actual values

--- app_create.sh
+++ app_create.sh
@@ -1,18 +1,25 @@
+#Note : Prequisites : A sample application should be registered with Azure AD prior to running this script
 
-REGION="us-east-1"
+
+REGION="REGION"
 
 # Role or user that will have permission over new AWS KMS CMK
-PRINCIPALARN="arn:aws:iam::972585204640:user/cloud_user"
-CLIENTID="871d759b-b74e-46b8-81c5-076d7ec70426"
-CLIENTSECRET="rZU2:.3Zr=lKt/W7ORCG3=Og6UC8V1yN"
+PRINCIPALARN="arn:aws:iam::<<ACCOUNT_ID>:user/<user>"
+CLIENTID="CLIENTID_VALUE" #Fill the Client ID value from Azure AD
+CLIENTSECRET="CLIENTSECRET_VALUE"
 PYTHON_PATH="python/lib/python3.7/site-packages"
 
+
+
+## Create Lambda Function Artefact
+zip -r9 lambda_function.zip auditcheck.py
 
 
 mkdir -p msal_lambda_layers/$PYTHON_PATH
 pip install msal --target msal_lambda_layers/$PYTHON_PATH/.
 cd msal_lambda_layers
 zip -r9 ../msal_package.zip *
+cd ..
 
 aws lambda publish-layer-version \
     --layer-name Azure_msal \
@@ -21,7 +28,9 @@ aws lambda publish-layer-version \
     --compatible-runtimes python3.7
 
 
-## Execute CloudFormation
+
+MSAL_VERSION_ARN=`aws lambda get-layer-version --layer-name Azure_msal --query LayerVersionArn`
+
 ##
 aws cloudformation create-stack --stack-name azureidamops  --template-body file://Azure_IDAMcftemplate.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=KMSAdminArn,ParameterValue=$PRINCIPALARN 
 
@@ -47,9 +56,8 @@ aws ssm put-parameter --name AzureGraphAPIClientSecret \
 
 RESULT=`aws iam get-role --role-name Custom-Lambda-AzureADLogs --query Role.Arn`
 ROLE=`echo $RESULT | sed 's/"//g'`
-REGION="us-east-1"
 
 aws lambda create-function --function-name azureidamfunction \
 --role $ROLE --handler auditcheck.lambda_handler \
 --runtime python3.7 --timeout 300 --zip-file fileb://lambda_function.zip --region $REGION \
---layers "arn:aws:lambda:us-east-1:972585204640:layer:Azure_msal:2" "arn:aws:lambda:us-east-1:972585204640:layer:httprequests:2"
+--layers $MSAL_VERSION_ARN
